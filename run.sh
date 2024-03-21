a82cb457ed2a0ca62ab7177b996993b27eb5ceab
sh1r01r0 <116267497+sh1r01r0@users.noreply.github.com>
2024-03-21 23:26:05 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-2787a89a-1f57-44f9-8d9e-f04eef6b6f6f/)
 
 
 echo "$curl_output" >> Update.tsx
